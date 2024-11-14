1240ac3429ba1b7dd89e610ee76f9a13d27f86ab
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-14 18:07:16 +0900

    검색바 완성

---
+++ src/pages/api/time.ts
@@ -0,0 +1,5 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const data = new Date();
+  res.json({ time: data.toLocaleDateString() });
+}
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+import SearchableLayout from "@/components/searchable-layout";
+export default function Page() {
+  const router = useRouter();
+  const { q } = router.query;
+  return <h1>Search{q}</h1>;
+}
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
