e86af71c60c56976c09ed93daaf83f0df9047b72
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-14 22:42:37 +0900

    feat : 비동기로 모든도서 불러오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,40 @@
 import style from "./index.module.css";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-export default function Home() {
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
+
+export const getServerSideProps = async () => {
+  const [allbooks, recobooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allbooks,
+      recobooks,
+    },
+  };
+};
+
+export default function Home({
+  allbooks,
+  recobooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recobooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allbooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
