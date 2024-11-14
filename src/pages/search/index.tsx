f6c7ca51a92006bd4ec3d9e834efbf0d67523edb
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-14 20:38:27 +0900

    feat : ui 구현완료

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,14 @@
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-  return <h1>Search{q}</h1>;
+  <div>
+    {books.map((book) => (
+      <BookItem key={book.id} {...book} />
+    ))}
+  </div>;
 }
 
 Page.getLayout = (page: ReactNode) => {
