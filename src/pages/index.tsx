c6a5eaf0d46cb966bdc87fdb0cb3d649f4e415a9
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-15 11:07:26 +0900

    feat : SSR랜더링 방식말고 SSG렌더링 방식으로 바꿈
    - 정적 사이드 렌더링 : 빌드 타입에 미리 페이지를 사전 렌더링 해두는 방식
    - 쓰이는 이유는 백엔드서버에서 데이터가 너무 많이 처리가 되거나 무슨 문제가 생겼을 시 크라이언트가 ux경험이 안좋아지기때문에 미리 페이지를 사전 렌더링해두는 방식이다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import style from "./index.module.css";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
+
   const [allbooks, recobooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -23,7 +25,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allbooks,
   recobooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
