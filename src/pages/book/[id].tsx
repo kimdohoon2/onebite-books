629e6e8328cbaa328b9105ebd2d3d03832652a89
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-11-15 11:42:47 +0900

    feat : SSR -> SSG로 변경, fallback option설정

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,12 +1,37 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import {
+  GetServerSidePropsContext,
+  GetStaticPropsContext,
+  InferGetServerSidePropsType,
+  InferGetStaticPropsType,
+} from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-book";
+import { useRouter } from "next/router";
+import { notFound } from "next/navigation";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: true,
+    // false : 404 NotFound
+    // blocking : SSR 방식
+    // true: SSR 방식 + 데이터가 없는 폴백 상태으이 페이지부터 변환
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: { book },
   };
@@ -14,7 +39,10 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  if (router.isFallback) return "로딩중입니다.";
+
   if (!book) return "문제가 발생했습니다. 다시 시작해주세요";
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
     book;
